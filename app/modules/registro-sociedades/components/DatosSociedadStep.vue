c4fc0176d8a41e12074b4133f659ef4e66ba4062
Esteve Paredes <esteve.paredes.4@gmail.com>
2025-10-21 16:50:24 -0500

    feat: se define la estructura de carpetas en base a los modulos de la app

---
+++ app/modules/registro-sociedades/components/DatosSociedadStep.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+interface Props {
+  mode: "crear" | "editar";
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div>Datos de la sociedad: {{ mode }}</div>
+</template>
---
+++ app/pages/registro-societario/crear/datos-sociedad.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import DatosSociedadStep from "~/modules/registro-sociedades/components/DatosSociedadStep.vue";
+</script>
+
+<template>
+  <DatosSociedadStep mode="crear" />
+</template>
