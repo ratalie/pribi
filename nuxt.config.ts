486b35998cfff1287666d739c85eb284715a794f
Yull Timoteo <yull.timoteo.z.23@gmail.com>
2025-10-27 14:05:41 -0500

    refactor: simplify index.vue template and update nuxt.config.ts for devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
-  devtools: { enabled: true },
+  devtools: { enabled: true, editor: "code" },
 
   ssr: false,
 
