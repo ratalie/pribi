b3e5864c76e2f39ac12a24fe59e7da817bef7008
Esteve Paredes <esteve.paredes.4@gmail.com>
2025-10-28 16:44:02 -0500

    feat: se agrego el esquema de elementos del nuevo componente layout para los flujos

--- app/layouts/flow-layout.vue
+++ app/layouts/flow-layout.vue
@@ -1,8 +1,25 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex flex-col gap-10">
-    <p>Flow Layout</p>
-    <slot />
+  <div class="flex flex-col h-screen">
+    <!-- Header -->
+    <div class="h-[104px] border-y">
+      <h1 class="text-2xl font-bold">Flow Layout</h1>
+    </div>
+    <!-- Body -->
+    <div class="flex min-h-0 flex-1">
+      <div class="w-[401px] border-r">
+        <p>Progress bar</p>
+      </div>
+
+      <div class="flex-1 overflow-y-auto">
+        <div class="flex-1">
+          <slot />
+        </div>
+        <div class="h-[92px] border-t sticky bottom-0 bg-white z-10 shrink-0">
+          <div>Footer content</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
