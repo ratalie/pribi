c2cbb4ea4e97847701c00f0bab09372237ce7510
Olenka <sanchezaguilarolenka@gmail.com>
2025-10-28 07:22:11 -0500

    add: card title reusable to accept buttons and switch

---
+++ app/components/base/cards/CardTitle.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex w-full" :class="$slots.actions ? 'justify-between items-start' : ''">
+    <div class="flex flex-col gap-2">
+      <div class="flex gap-2 items-center">
+        <p class="text-[26px] leading-[28px] text-gray-800 font-semibold font-primary">
+          {{ title }}
+        </p>
+        <slot name="switch" />
+      </div>
+      <p
+        v-if="body"
+        class="text-[14px] leading-[16px] font-secondary text-gray-600 font-medium"
+      >
+        {{ body }}
+      </p>
+    </div>
+    <slot name="actions" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+  defineProps({
+    title: { type: String, default: "Title" },
+    body: { type: String, default: "Body" },
+  });
+</script>
