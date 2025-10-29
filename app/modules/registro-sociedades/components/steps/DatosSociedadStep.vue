100913eba6f8ccef62a7ccc216d6e75d73e9fe8f
Olenka <sanchezaguilarolenka@gmail.com>
2025-10-28 21:04:23 -0500

    add: switch example

--- app/modules/registro-sociedades/components/steps/DatosSociedadStep.vue
+++ app/modules/registro-sociedades/components/steps/DatosSociedadStep.vue
@@ -6,7 +6,7 @@
   import SearchInputZod from "~/components/base/inputs/text/ui/SearchInputZod.vue";
   import SelectInputZod from "~/components/base/inputs/text/ui/SelectInputZod.vue";
   import TextInputZod from "~/components/base/inputs/text/ui/TextInputZod.vue";
-  import SwitchTabs from "~/components/base/Switch/SwitchTabs.vue";
+  import SimpleSwitchYesNo from "~/components/base/Switch/SimpleSwitchYesNo.vue";
   import Switch from "~/components/ui/switch/Switch.vue";
   import { officeOptions } from "~/constants/inputs/office-options";
   import { societyTypeOptions } from "~/constants/inputs/society-types";
@@ -88,6 +88,7 @@
   <div class="min-h-screen">
     <!-- Formulario principal -->
     <div class="bg-white p-8">
+      <SimpleSwitchYesNo />
       <!-- Ejemplo 2: Con un botón -->
       <CardTitle
         title="Datos principales"
