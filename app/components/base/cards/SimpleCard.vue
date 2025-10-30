4f4ee999cdc63be7cbc0a407870d68b1da1e013c
Olenka <sanchezaguilarolenka@gmail.com>
2025-10-30 13:17:47 -0500

    add: basic card component

---
+++ app/components/base/cards/SimpleCard.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  
+</script>
+
+<template>
+  <div
+    class="h-full border border-gray-100 rounded-xl flex flex-col gap-12 py-12 px-10"
+  >
+    <slot />
+</div>
+</template>
