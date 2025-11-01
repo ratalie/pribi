8df8a1cf790ec593ab10425db6b37f4672218f46
Olenka <sanchezaguilarolenka@gmail.com>
2025-10-31 20:03:31 -0500

    fix: prettier

--- app/modules/registro-sociedades/schemas/quorumMayoria.ts
+++ app/modules/registro-sociedades/schemas/quorumMayoria.ts
@@ -1,7 +1,6 @@
 import { z } from "zod";
 
-export const porcentajeSchema = z
-  .string();
+export const porcentajeSchema = z.string();
 export const datosSociedadSchema = z.object({
   porcentaje: porcentajeSchema,
 });
