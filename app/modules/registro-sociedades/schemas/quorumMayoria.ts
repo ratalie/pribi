6dd57319c331da448347b38e5777a2969ebac0fc
Olenka <sanchezaguilarolenka@gmail.com>
2025-10-31 20:02:29 -0500

    fix: zod validation just string

--- app/modules/registro-sociedades/schemas/quorumMayoria.ts
+++ app/modules/registro-sociedades/schemas/quorumMayoria.ts
@@ -1,18 +1,7 @@
 import { z } from "zod";
 
 export const porcentajeSchema = z
-  .string()
-  .nonempty("")
-  .refine(
-    (val) => {
-      const num = parseFloat(val);
-      return !isNaN(num) && num > 0 && num <= 100;
-    },
-    {
-      message: "",
-    }
-  );
-
+  .string();
 export const datosSociedadSchema = z.object({
   porcentaje: porcentajeSchema,
 });
