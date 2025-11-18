a5a9e7affa3c1fcfdca708afd98b7c2cec637e9e
Olenka <sanchezaguilarolenka@gmail.com>
2025-11-17 21:19:43 -0500

    fix: clean quorumsMayoriasStep file

---
+++ app/pages/registro-societario/sociedades/crear/quorum-mayoria.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import QuorumMayoriaManager from "~/core/presentation/registros/sociedades/pasos/quorum/QuorumMayoriaManager.vue";
+  import { EntityModeEnum } from "~/types/enums/EntityModeEnum";
+
+  definePageMeta({
+    flowLayout: true,
+  });
+</script>
+
+<template>
+  <QuorumMayoriaManager :mode="EntityModeEnum.CREAR" />
+</template>
