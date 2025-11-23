9592110261ff18f15bb4b9d0d7744c5387312890
Esteve Paredes <esteve.paredes.4@gmail.com>
2025-11-23 12:26:08 -0500

    feat: se agrego la navegacion entre paginas mediante las rutas declaradas en el progressbar

--- app/composables/useFlowLayoutNext.ts
+++ app/composables/useFlowLayoutNext.ts
@@ -4,12 +4,22 @@ type FlowNextHandler = (() => void) | (() => Promise<void>);
 
 export const useFlowLayoutNext = (handleNext: FlowNextHandler) => {
   const flowLayoutStore = useFlowLayoutStore();
+  const progressNavbarStore = useProgressNavbarStore();
+  const router = useRouter();
+  const route = useRoute();
 
   onMounted(() => {
     flowLayoutStore.onClickNext = async () => {
       try {
         flowLayoutStore.isLoading = true;
         await handleNext();
+
+        // navegamos al siguiente paso
+        const nextStep = progressNavbarStore.getNextStepByCurrentStep(route.path);
+
+        if (nextStep) {
+          router.push(nextStep.route);
+        }
       } catch (error) {
         console.error(error);
       } finally {
